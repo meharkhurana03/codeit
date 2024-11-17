291bba880abcaa665beea09f98b0f810b5034cb7
Mehar Khurana <62463007+meharkhurana03@users.noreply.github.com>
2024-11-17 12:42:01 +0530

    path fix __init__.py

--- codeit/__init__.py
+++ codeit/__init__.py
@@ -5,4 +5,4 @@ import os
 
 location_init_file = os.path.dirname(os.path.realpath(__file__))
 # PROJECT_FOLDER_PATH = location_init_file.split("codeit")[0]
-PROJECT_FOLDER_PATH = location_init_file.split("/")[:-1]
+PROJECT_FOLDER_PATH = "/".join(location_init_file.split("/")[:-1])
